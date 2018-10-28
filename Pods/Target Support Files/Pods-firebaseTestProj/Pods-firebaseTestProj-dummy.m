594465b00600401f8a748abc8173541172f0e411
Michael Murphy <mpmurphy1432@gmail.com>
2018-10-28 13:44:13 -0500

    commit/push

---
+++ Pods/Target Support Files/Pods-firebaseTestProj/Pods-firebaseTestProj-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_firebaseTestProj : NSObject
+@end
+@implementation PodsDummy_Pods_firebaseTestProj
+@end
--- firebaseTestProj/ViewController.swift
+++ firebaseTestProj/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import Firebase
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteLbl: UILabel!
+    @IBOutlet weak var quoteTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    
+    var docRef: DocumentReference!
+    var quoteListener: ListenerRegistration!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        docRef = Firestore.firestore().document("sampleData/inspiration")
     }
-
+    
+    
+    @IBAction func fetchPressed(_ sender: UIButton) {
+        docRef.getDocument { (docSnapshot, error) in
+            guard let docSnapshot = docSnapshot, docSnapshot.exists else {return}
+            let myData = docSnapshot.data()
+            let latestQuote = myData?["quote"] as? String ?? ""
+            let quoteAuthor = myData?["author"] as? String ?? ""
+            self.quoteLbl.text = "\"\(latestQuote)\" -- \(quoteAuthor)"
+         }
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        docRef.addSnapshotListener { (docSnapshot, error) in
+            guard let docSnapshot = docSnapshot, docSnapshot.exists else {return}
+            let myData = docSnapshot.data()
+            let latestQuote = myData?["quote"] as? String ?? ""
+            let quoteAuthor = myData?["author"] as? String ?? ""
+            self.quoteLbl.text = "\"\(latestQuote)\" -- \(quoteAuthor)"
+        }
+        
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        quoteListener.remove()
+    }
+    
+    @IBAction func saveBtnPressed(_ sender: UIButton) {
+        guard let quoteText = quoteTextField.text, !quoteText.isEmpty else {return}
+        guard let quoteAuthor = quoteTextField.text, !quoteAuthor.isEmpty else {return}
+        let dataToSave: [String: Any] = ["quote": quoteText, "author": quoteAuthor]
+        docRef.setData(dataToSave) { (error) in
+            print(error)
+        }
+        
+        
+        
+    }
+    
 
 }
 
