375057937b452aa9bc09f2cbd8ac3f6df860c0d1
Michael Murphy <mpmurphy1432@gmail.com>
2018-10-28 12:51:15 -0500

    Initial Commit

---
+++ firebaseTestProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseTestProj
+//
+//  Created by Michael  Murphy on 10/28/18.
+//  Copyright © 2018 Michael  Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
